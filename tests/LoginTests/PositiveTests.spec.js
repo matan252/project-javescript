9d764d5e990c407f2f9ea66b18d6a58006994093
Matan <dogball11@gmail.com>
2024-11-24 11:53:19 +0200

    fixed

--- tests/LoginTests/PositiveTests.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-const Users = ['standard_user', 'locked_out_user', 'problem_out_user', 'preformance_glitch_user', 'error_user', 'visual_user'];
-
-for (let i=1; i <=6; i++){
-    function test
-}
-;
