62cc3af20931be457529540009d2875d28c75854
Netanel Harush <netanelharush2@gmail.com>
2024-11-24 14:43:34 +0200

    code review

--- tests/Sanity/Sanity.spec.js
+++ tests/Sanity/Sanity.spec.js
@@ -1,28 +1,29 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test'
 
-test('Buying items', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await expect(page.getByText('Swag Labs')).toBeVisible();
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.getByText('Products')).toBeVisible();
-  await page.getByText('Sauce Labs Backpack').click();
-  await page.locator('[data-test="add-to-cart"]').click();
-  await page.locator('[data-test="back-to-products"]').click();
-  await page.getByText('Sauce Labs Bike Light').click();
-  await page.locator('[data-test="add-to-cart"]').click();
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  await page.locator('[data-test="checkout"]').click();
-  await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill('matan');
-  await page.locator('[data-test="firstName"]').press('Tab');
-  await page.locator('[data-test="lastName"]').fill('rushdi');
-  await page.locator('[data-test="lastName"]').press('Tab');
-  await page.locator('[data-test="postalCode"]').fill('54321542');
-  await page.locator('[data-test="continue"]').click();
-  await page.locator('[data-test="finish"]').click();
-  await page.locator('[data-test="back-to-products"]').click();
-});
+test('Buying items', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/')
+    await expect(page.getByText('Swag Labs')).toBeVisible()
+    await page.locator('[data-test="username"]').click()
+    await page.locator('[data-test="username"]').fill('standard_user')
+    await page.locator('[data-test="password"]').click()
+    await page.locator('[data-test="password"]').fill('secret_sauce')
+    await page.locator('[data-test="login-button"]').click()
+    await expect(page.getByText('Products')).toBeVisible()
+    await page.getByText('Sauce Labs Backpack').click()
+    await page.locator('[data-test="add-to-cart"]').click()
+    await page.locator('[data-test="back-to-products"]').click()
+    await page.getByText('Sauce Labs Bike Light').click()
+    await page.locator('[data-test="add-to-cart"]').click()
+    await page.locator('[data-test="shopping-cart-link"]').click()
+    // where is the validation that the amount of the selected items are present in the checkout overview?
+    await page.locator('[data-test="checkout"]').click()
+    await page.locator('[data-test="firstName"]').click()
+    await page.locator('[data-test="firstName"]').fill('matan')
+    await page.locator('[data-test="firstName"]').press('Tab')
+    await page.locator('[data-test="lastName"]').fill('rushdi')
+    await page.locator('[data-test="lastName"]').press('Tab')
+    await page.locator('[data-test="postalCode"]').fill('54321542')
+    await page.locator('[data-test="continue"]').click()
+    await page.locator('[data-test="finish"]').click()
+    await page.locator('[data-test="back-to-products"]').click()
+})
