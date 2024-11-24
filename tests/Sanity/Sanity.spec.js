4295c25ad365e71403b5955d241b1f02b437a66b
Matan <dogball11@gmail.com>
2024-11-24 14:01:56 +0200

    Working now

--- tests/Sanity/Sanity.spec.js
+++ tests/Sanity/Sanity.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Buying items', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await expect(page.getByText('Swag Labs')).toBeVisible();
   await page.locator('[data-test="username"]').click();
@@ -9,11 +9,11 @@ test('test', async ({ page }) => {
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
   await expect(page.getByText('Products')).toBeVisible();
-  await page.getByRole('Sauce Labs Backpack').click();
-  await page.getByRole('Add to cart').click();
-  await page.getByRole('Back to products').click();
-  await page.getByRole('Sauce Labs Bike Light').click();
-  await page.getByRole('Add to cart').click();
+  await page.getByText('Sauce Labs Backpack').click();
+  await page.locator('[data-test="add-to-cart"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.getByText('Sauce Labs Bike Light').click();
+  await page.locator('[data-test="add-to-cart"]').click();
   await page.locator('[data-test="shopping-cart-link"]').click();
   await page.locator('[data-test="checkout"]').click();
   await page.locator('[data-test="firstName"]').click();
